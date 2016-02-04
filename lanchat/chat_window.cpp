42ff3e7521688bb0d50dca3b74d6847c978ea0b3
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2016-02-04 10:00:56 +0300

    [NOT FOR MASTER] Checking chat message widgets

--- lanchat/chat_window.cpp
+++ lanchat/chat_window.cpp
@@ -1,3 +1,6 @@
+#include "chat_incoming_message.h"
+#include "chat_outgoing_message.h"
+
 #include "chat_window.h"
 #include "ui_chat_window.h"
 
@@ -6,6 +9,22 @@ ChatWindow::ChatWindow(QWidget *parent) :
   ui(new Ui::ChatWindow)
 {
   ui->setupUi(this);
+
+  QTreeWidgetItem *item = new QTreeWidgetItem();
+  ui->listHistory->addTopLevelItem(item);
+  ui->listHistory->setItemWidget(item, 0, new ChatIncomingMessage());
+
+  item = new QTreeWidgetItem();
+  ui->listHistory->addTopLevelItem(item);
+  ui->listHistory->setItemWidget(item, 0, new ChatIncomingMessage());
+
+  item = new QTreeWidgetItem();
+  ui->listHistory->addTopLevelItem(item);
+  ui->listHistory->setItemWidget(item, 0, new ChatOutgoingMessage());
+
+  item = new QTreeWidgetItem();
+  ui->listHistory->addTopLevelItem(item);
+  ui->listHistory->setItemWidget(item, 0, new ChatIncomingMessage());
 }
 
 ChatWindow::~ChatWindow()
